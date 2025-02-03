2f69589b5ee1e883ba60b3332ba795bd8a2cb86f
BruunoA <bandia@inscaparrella.cat>
2025-02-03 17:08:01 +0100

    dd
    dd

---
+++ js/sketch2.js
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+function setup() {
+  createCanvas(600, 400).parent("#sketch-holder");
+}
+
+/**
+ *
+ */
+function draw() {
+  background(135, 206, 235);
+  fill("yellow"); ñ;
+  stroke("orange");
+  strokeWeight(20);
+  circle(550, 50, 100);
+}
+
+globalThis.setup = setup;
+globalThis.draw = draw;
+globalThis.preload=preload;
